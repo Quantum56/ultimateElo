7657c454cf0ba4445daebf7c10217ee2d51c288b
Quantum56 <bartleyzack@gmail.com>
2020-04-20 15:46:12 -0400

    testing

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import numpy as np
+
+
+def main():
+    fd = emptyArr(5)
+    for i in range(len(emptyArr)
+        
+
+def updateElo(arr):
+    return 
+    
+def emptyArr(na):
+    li = []
+    ia = 0
+    for l in range(na):
+        li.append([])
+    for la in range(na):
+        for ls in range(na):
+            li[ia].append(na)
+        ia+=1
+    return li
+
+if __name__=='__main__':
+    main()
