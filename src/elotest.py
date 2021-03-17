53814164a340f1b8ad95a63a6e248def0dd07db4
Quantum56 <bartleyzack@gmail.com>
2021-03-17 01:48:25 -0400

    updated

--- src/elotest.py
+++ src/elotest.py
@@ -2,7 +2,7 @@ import os
 import math as m
 import array
 import time
-import numpy
+import numpy as np
 from numpy import random as r
 import scipy.integrate as integrate
 
@@ -33,28 +33,44 @@ def main():
     test1[:] = [3000*t for t in test1]
     test1.sort()
     print(test1)
-    print(numpy.mean(test1))
-
+    print(np.mean(test1))
+#    print("Actual: " + str(prob_win(1400, 1600)))
+    E = 1/(1 + 10**((1600 - 1400)/400))
+    print("Calculated: " + str(E))
+    #print("Distribution: " + str(np.mean([r.rand() for i in range(1000)])))
+    
+#def prob_win(Ra, Rb): # returns probability of win for A
+#    try:
+#        return ((1/700)*((Ra - Rb)) + 0.5)*(1 + r.rand()/8)
+#    except ZeroDivisionError:
+#        return 0.5
+    
 
 def normal(x):
-    return (0.5 + SQRTPI_INV*float(integrate.quad(lambda t: m.exp(-(t**2)), 0, x)[0]))
+    return (0.5 + SQRTPI_INV * float(integrate.quad(lambda t: m.exp(-(t**2)), 0, x)[0]))
 
 def test():
     print("test\n")
-    t_start = time.time()
-    i = 0
-    while (i < 1000):
-        test = update_elo(1400, 1200, -1)
-        i = i + 1
-    t_end = time.time()
+    #t_start = time.time()
+    #i = 0
+    #while (i < 1000):
+    #    test = play_game(1400, 1300)
+    #    i = i + 1
+    #t_end = time.time()
+    test = play_game(1400, 1300)
     print(str(test[0]) + " = Ra new \n" + str(test[1]) + " = Rb new\n")
-    print(str(t_end - t_start) + " millis")
+    #print(str(t_end - t_start) + " millis")
 
-
-def update_elo(Ra, Rb, result): # if result = 1: A won, and if result = -1: B won
-    temp = []
+def prob(Ra, Rb):
     Ea = 1/(1 + 10**((Rb - Ra)/400))
     Eb = 1/(1 + 10**((Ra - Rb)/400))
+    return (Ea, Eb)
+
+def update_elo(Ra, Rb, result, Ea, Eb): # if result = 1: A won, and if result = -1: B won
+    temp = []
+    #Ea = 1/(1 + 10**((Rb - Ra)/400))
+    #Eb = 1/(1 + 10**((Ra - Rb)/400))
+#    Ea, Eb = prob(Ra, Rb)[0], prob(Ra, Rb)[1]
     if (result == 1):
         a_res = 1
         b_res = 0
@@ -68,6 +84,16 @@ def update_elo(Ra, Rb, result): # if result = 1: A won, and if result = -1: B wo
     temp.append(Ra_new)
     temp.append(Rb_new)
     return temp
+
+def play_game(Ra, Rb):
+    result = r.rand()
+    Ea, Eb = prob(Ra, Rb)[0], prob(Ra, Rb)[1]
+    if (result <= Ea):
+        result = 1
+    else:
+        result = -1
+    return update_elo(Ra, Rb, result, Ea, Eb)
+
 
 if (__name__ == "__main__"):
     main()
---
+++ src/helpers.py
@@ -0,0 +1,41 @@
+import math as m
+import numpy as np
+from numpy import random as r
+import scipy.integrate as integrate
+
+SQRTPI_INV = 1/m.sqrt(m.pi)
+
+def normal(x):
+    return (0.5 + SQRTPI_INV * float(integrate.quad(lambda t: m.exp(-(t**2)), 0, x)[0]))
+
+def test():
+    print("test\n")
+    test = play_game(1400, 1300)
+    print(str(test[0]) + " = Ra new \n" + str(test[1]) + " = Rb new\n")
+
+def prob(Ra, Rb):
+    Ea = 1/(1 + 10**((Rb - Ra)/400))
+    Eb = 1/(1 + 10**((Ra - Rb)/400))
+    return (Ea, Eb)
+
+def update_elo(Ra, Rb, result, Ea, Eb): # if result = 1: A won, and if result = -1: B won
+    if (result == 1):
+        a_res = 1
+        b_res = 0
+    elif (result == -1):
+        a_res = 0
+        b_res = 1
+    else:
+        raise ValueError("result was not -1 or 1")
+    Ra_new = Ra + 32*(a_res - Ea)
+    Rb_new = Rb + 32*(b_res - Eb)
+    return (Ra_new, Rb_new)
+
+def play_game(Ra, Rb):
+    result = r.rand()
+    Ea, Eb = prob(Ra, Rb)[0], prob(Ra, Rb)[1]
+    if (result <= Ea):
+        result = 1
+    else:
+        result = -1
+    return update_elo(Ra, Rb, result, Ea, Eb)
